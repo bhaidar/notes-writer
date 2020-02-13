124192e0f7de97cc8e525d75078841999feda1bb
Bilal Haidar <bhaidar@gmail.com>
2020-02-14 01:42:18 +0200

    Send email using nodemailer

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,45 @@
 const functions = require('firebase-functions')
+const admin = require('firebase-admin')
+const nodemailer = require('nodemailer')
 
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send('Hello from Firebase!')
+admin.initializeApp()
+
+var transporter = nodemailer.createTransport({
+  host: 'smtp.gmail.com',
+  port: 465,
+  secure: true,
+  auth: {
+    user: '',
+    pass: ''
+  }
 })
+
+exports.onNewNoteCreated =
+  functions.firestore.document('notes/{notesId}').onCreate((snap, context) => {
+    const { body } = snap.data()
+    const { params: { notesId } } = context
+
+    console.log(body)
+    console.log(notesId)
+
+    const mailOptions = {
+      from: process.env.VUE_APP_EMAIL,
+      to: 'bhaidar@gmail.com',
+      subject: 'A new note is created',
+      html: `
+        <h1>New Note</h1>
+        <p>A new note is created with the following details:</p>
+        <p>
+          Note Id: <br />
+          ${notesId}
+        </p>
+        <p>
+          Note Body: <br />
+          ${body}
+        </p>`
+    }
+
+    return transporter.sendMail(mailOptions)
+      .then(() => console.log('Email Sent!'))
+      .catch(error => console.error(error))
+  })
